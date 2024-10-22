f868508219ae56a1272e2711a7538927193ac95d
Glaz8212 <lws7129@naver.com>
2024-10-22 12:50:53 +0900

    Feat : 커브볼 추가 구현
    angularVelocity(각속도) z추가
    
    피칭 머신이 랜덤으로 직구, 커브 중 하나 투구

--- Assets/Scripts/PitchingMachine.cs
+++ Assets/Scripts/PitchingMachine.cs
@@ -6,21 +6,49 @@ public class PitchingMachine : MonoBehaviour
 {
     public GameObject ballPrefab;
     public Transform spawnPoint;
-    public float pitchingForce;
+    public float fourSeamForce; // 직구
+    public float curveForce; // 커브
     public float pitchInterval; // 투구 사이 간격
 
     private void Start()
     {
-        InvokeRepeating("PitchBall", 2f, pitchInterval); // 2초후부터 인터벌마다 발사
+        InvokeRepeating("ThrowRandomPitch", 2f, pitchInterval); // 2초후부터 인터벌마다 발사
     }
 
-    private void PitchBall()
+    private void ThrowRandomPitch()
+    {
+        int pitchType = Random.Range(0, 2);
+
+        switch (pitchType)
+        {
+            case 0:
+                ThrowFourSeam();
+                break;
+            case 1:
+                ThrowCurveBall();
+                break;
+        }
+    }
+
+    // 직구
+    private void ThrowFourSeam()
     {
         GameObject ball = Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+        ballRigidbody.AddForce(spawnPoint.forward * fourSeamForce, ForceMode.Impulse);
+        Destroy(ball, 10f);
+    }
 
-        Rigidbody rigid = ball.GetComponent<Rigidbody>();
-        rigid.AddForce(spawnPoint.forward * pitchingForce, ForceMode.Impulse);
+    // 커브 (아래로 향하는 궤적)
+    private void ThrowCurveBall()
+    {
+        GameObject ball = Instantiate(ballPrefab, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+        ballRigidbody.AddForce(spawnPoint.forward * curveForce, ForceMode.Impulse);
 
-        Destroy(ball, 10f); // 볼 10초 후 삭제
+        // 아래로 떨어지도록 회전력 추가
+        ballRigidbody.angularVelocity = new Vector3(0, 0, 5f);
+
+        Destroy(ball, 10f);
     }
 }
